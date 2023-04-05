eafddf164265d73844bb3ba144f96ab166dd4c1d
chappi17 <kiralnm2@gmail.com>
2023-04-05 22:31:11 +0900

    add Programmers

--- algorithm/0.cpp
+++ algorithm/0.cpp
@@ -5,14 +5,28 @@
 using namespace std;
 int main()
 {
-	vector<int> num_list = {1,1,1,1,1,2};
+	vector<int> num_list = { 1,3,5,7 };
+	int count = 0;
 	vector<int> answer;
-	
-	for (int i = 0; i < 1; i++)
-	{
-		reverse(num_list.begin(), num_list.end());
-	}
-	answer = num_list;
 
+	for (int i = 0; i < num_list.size(); i++)
+	{
+		if (num_list[i] % 2 == 0)
+		{
+			count++;
+		}
+	}
+	answer.push_back(count);
+	count = 0;
+
+	for (int i = 0; i < num_list.size(); i++)
+	{
+		if (num_list[i] % 2 != 0)
+		{
+			count++;
+		}
+	}
+	answer.push_back(count);
+	
 	return 0;
 }
