0da8b9ead1b8ca7a41862cdf6849674211f5e12b
chappi17 <kiralnm2@gmail.com>
2023-04-11 22:23:25 +0900

    add programmers

---
+++ algorithm/7/10757.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <ctype.h>
+
+using namespace std;
+
+// Å«¼ö A + B
+int main()
+{
+	long long A = 9223372036854775807;
+	long long B = 9223372036854775808;
+
+	cout << A + B << endl;
+	return A + B;
+}
--- algorithm/Programmers/26.cpp
+++ algorithm/Programmers/26.cpp
@@ -8,21 +8,20 @@
 
 using namespace std;
 
+// 중앙값 구하기
 int main()
 {
-	string word;
-	string note[100];
-	cin >> word;
+	vector<int> array = { 1,2,7,10,11 };
+	int answer = 0;
 
-	for (int i = 0; i < word.size(); i++)
-	{
-		note[i]+= tolower(word[i]);
-	}
+	sort(array.begin(), array.end());
 
-	for (int i = 0; i < word.size(); i++)
-	{
+	answer=array[array.size() / 2];
 
-	}
+
+
 
 	return 0;
 }
+
+
