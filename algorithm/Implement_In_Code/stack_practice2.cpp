39329f48aae9d604b7dc0c3cf75d1478ea64bde8
Parhelion <93296510+chappi17@users.noreply.github.com>
2024-02-21 21:27:02 +0900

    Data Structure Practice
    - stack

--- algorithm/Implement_In_Code/stack_practice2.cpp
+++ algorithm/Implement_In_Code/stack_practice2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <stack>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,10 +22,49 @@ string reverseString(const string& str)
 	}
 	return result;
 }
+bool isBalancedParentheses(const string& parentheses)
+{
+	stack<char> a;
 
+	for (char b : parentheses)
+	{
+		if (b == '(' || a.empty())
+		{
+			a.push(b);
+		}
+		else if (b == ')' && !a.empty())
+		{
+			a.pop();
+		}
+	}
+	return a.empty() ? true : false;
+}
+// 아직 미해결
+void sortStack(stack<int>& inputStack)
+{
+
+
+}
 int main()
 {
-	string b = "hello" ;
-	reverseString(b);
+	//string b = "hello" ;
+	////reverseString(b);
+	//string c = ")(";
+	//cout << isBalancedParentheses(c) ? true : false;
+	stack<int> a;
+	a.push(1);
+	a.push(7);
+	a.push(2);
+	a.push(5);
+
+	sortStack(a);
+
+
+
+	while (!a.empty())
+	{
+		cout << a.top() << "\n";
+		a.pop();
+	}
 	return 0;
 }
